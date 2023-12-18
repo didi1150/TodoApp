a5c1a8517837f5ace33c5d5ff97e3891864d13a2
unknown <didicomputing@gmail.com>
2023-12-18 16:52:06 +0100

    Fixed expandabl fab not working

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:Todos/components/TodoEntrySection.dart';
 import 'package:Todos/model/CategoryEntry.dart';
 import 'package:Todos/model/TodoEntry.dart';
 import 'package:Todos/model/state/CategoryManager.dart';
+import 'package:draggable_expandable_fab/draggable_expandable_fab.dart';
 import 'package:uuid/uuid.dart';
 
 void main() async {
@@ -84,146 +85,150 @@ class _TodoAppState extends State<TodoApp> {
 
   @override
   Widget build(BuildContext context) {
-    return FloatingDraggableWidget(
-      mainScreenWidget: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-          title: Text(
-            "Todo App",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).appBarTheme.foregroundColor,
-                fontSize: 25),
-          ),
-          bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(4.0),
-              child: Container(
-                color: Colors.blue,
-                height: 2.0,
-              )),
-          elevation: 10,
-          centerTitle: true,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+        title: Text(
+          "Todo App",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).appBarTheme.foregroundColor,
+              fontSize: 25),
         ),
-        body: FutureBuilder(
-            future: Future.wait([
-              SQLiteStorage().getCategories(),
-              SQLiteStorage().getAllTodoAmounts(),
-              SQLiteStorage().getAllTodos()
-            ]),
-            builder: (context, snapshot) {
-              List<Widget> children;
-
-              if (snapshot.hasError) {
-                children = <Widget>[
-                  const Icon(
-                    Icons.error_outline,
-                    color: Colors.red,
-                    size: 60,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Text('Error: ${snapshot.error}'),
-                  ),
-                ];
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: children,
-                  ),
-                );
-              } else if (!snapshot.hasData) {
-                children = const <Widget>[
-                  SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: CircularProgressIndicator(),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 16),
-                    child: Text('Awaiting result...'),
-                  ),
-                ];
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: children,
-                  ),
-                );
-              }
-
-              List<CategoryEntry> categories =
-                  snapshot.data![0] as List<CategoryEntry>;
-              Map<String, int> amountsMap =
-                  snapshot.data![1] as Map<String, int>;
-              Map<String, List<TodoEntry>> todosMap =
-                  snapshot.data![2] as Map<String, List<TodoEntry>>;
-
-              categoryManager.possibleCategories = categories;
-              if (categories.isNotEmpty) {
-                categoryManager.selectedEntry ??= categories[0];
-              }
-
-              var currentTodos = [];
-              if (categoryManager.selectedEntry != null)
-                currentTodos =
-                    todosMap[categoryManager.selectedEntry!.name] ?? [];
-              todoList = [...currentTodos];
-              _searchResults = List.from(todoList);
-
-              return SingleChildScrollView(
-                  child: SizedBox(
-                height: MediaQuery.of(context).size.height -
-                    Scaffold.of(context).appBarMaxHeight!.toInt(),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Padding(
-                        padding: EdgeInsets.only(left: 5),
-                        child: Text("Categories",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 25))),
-                    CategorySection(
-                      categoryManager: categoryManager,
-                      amountsMap: amountsMap,
-                      updateFunction: () {
-                        setState(() {});
-                      },
-                      select: _selectCategory,
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    TodoEntrySearchBar(
-                      callback: updateSearchResult,
-                    ),
-                    const SizedBox(height: 15),
-                    TodoEntrySection(
-                      selectedCategory: categoryManager.selectedEntry,
-                      searchResults: [..._searchResults],
-                      updateFunction: () {
-                        setState(() {});
-                      },
-                    ),
-                  ],
-                ),
-              ));
-            }),
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(4.0),
+            child: Container(
+              color: Colors.blue,
+              height: 2.0,
+            )),
+        elevation: 10,
+        centerTitle: true,
       ),
-      floatingWidget: CreateFABButton(distance: 112, children: [
-        ActionButton(
-          icon: const Icon(Icons.create_new_folder),
-          onPressed: () => _showAction(context, 0),
-        ),
-        GestureDetector(
-            child: ActionButton(
-          icon: const Icon(Icons.add),
-          onPressed: () => _showAction(context, 1),
-        ))
-      ]),
-      floatingWidgetHeight: 60,
-      floatingWidgetWidth: 60,
+      body: FutureBuilder(
+          future: Future.wait([
+            SQLiteStorage().getCategories(),
+            SQLiteStorage().getAllTodoAmounts(),
+            SQLiteStorage().getAllTodos()
+          ]),
+          builder: (context, snapshot) {
+            List<Widget> children;
+
+            if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                ),
+              ];
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
+                ),
+              );
+            } else if (!snapshot.hasData) {
+              children = const <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting result...'),
+                ),
+              ];
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
+                ),
+              );
+            }
+
+            List<CategoryEntry> categories =
+                snapshot.data![0] as List<CategoryEntry>;
+            Map<String, int> amountsMap = snapshot.data![1] as Map<String, int>;
+            Map<String, List<TodoEntry>> todosMap =
+                snapshot.data![2] as Map<String, List<TodoEntry>>;
+
+            categoryManager.possibleCategories = categories;
+            if (categories.isNotEmpty) {
+              categoryManager.selectedEntry ??= categories[0];
+            }
+
+            var currentTodos = [];
+            if (categoryManager.selectedEntry != null)
+              currentTodos =
+                  todosMap[categoryManager.selectedEntry!.name] ?? [];
+            todoList = [...currentTodos];
+            _searchResults = List.from(todoList);
+
+            return SingleChildScrollView(
+                child: SizedBox(
+              height: MediaQuery.of(context).size.height -
+                  Scaffold.of(context).appBarMaxHeight!.toInt(),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Padding(
+                      padding: EdgeInsets.only(left: 5),
+                      child: Text("Categories",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25))),
+                  CategorySection(
+                    categoryManager: categoryManager,
+                    amountsMap: amountsMap,
+                    updateFunction: () {
+                      setState(() {});
+                    },
+                    select: _selectCategory,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TodoEntrySearchBar(
+                    callback: updateSearchResult,
+                  ),
+                  const SizedBox(height: 15),
+                  TodoEntrySection(
+                    selectedCategory: categoryManager.selectedEntry,
+                    searchResults: [..._searchResults],
+                    updateFunction: () {
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
+            ));
+          }),
+      floatingActionButtonAnimator: NoScalingAnimation(),
+      floatingActionButtonLocation: ExpandableFloatLocation(),
+      floatingActionButton: ExpandableDraggableFab(
+          childrenTransition: ChildrenTransition.scaleTransation,
+          distance: 150,
+          childrenBacgroundColor: Colors.transparent,
+          childrenCount: 2,
+          childrenType: ChildrenType.rowChildren,
+          enableChildrenAnimation: true,
+          childrenAlignment: Alignment.bottomLeft,
+          children: [
+            ActionButton(
+              icon: const Icon(Icons.create_new_folder),
+              onPressed: () => _showAction(context, 0),
+            ),
+            ActionButton(
+              icon: const Icon(Icons.add),
+              onPressed: () => _showAction(context, 1),
+            )
+          ]),
     );
   }
 }
